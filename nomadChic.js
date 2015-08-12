589e59694aad389efad71847cc2e575ab2c1a323
IsabelBru <izzibru@gmail.com>
2015-08-12 15:32:34 -0700

    Working on the lists

--- nomadChic.js
+++ nomadChic.js
@@ -1,5 +1,79 @@
 //api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+
+var gender;
+var temp;
+
 function packingList(){
 	var gender= document.getElementById("who").value;
  	console.log(gender);
+}
+
+
+function tempList(){
+	var temp= document.getElementById("tempe").value;
+ 	console.log(temp);
 }	
+
+function makeList(){
+	//Put the essentials here*******
+	//Call Medical
+	//Call Hygiene
+	//Call Electronics
+	//This is for if it is a female
+	if (gender=="Female") {
+		//Call fEssentials
+		//Call fHygiene
+		//This is for the temperature Blazing
+		if (temp=="B"){
+
+		}
+		//This is for the temperature Hot
+		else if (temp=="H"){
+
+		}
+		//This will pull the list for Warm
+		else if (temp=="W"){
+
+		}
+		//This will pull the list for Cool
+		else if (temp=="Cl"){
+
+		}
+		//This will pull the list for Cold
+		else if (temp=="Cd"){
+
+		}
+		//This will pull the list for Frigid
+		else{
+
+		}
+	}
+	//This is for if it is a male
+	else {
+		//Call mEssentials
+		//This is for the temperature Blazing
+		if (temp=="B"){
+
+		}
+		//This is for the temperature Hot
+		else if (temp=="H"){
+
+		}
+		//This will pull the list for Warm
+		else if (temp=="W"){
+
+		}
+		//This will pull the list for Cool
+		else if (temp=="Cl"){
+
+		}
+		//This will pull the list for Cold
+		else if (temp=="Cd"){
+
+		}
+		//This will pull the list for Frigid
+		else{
+
+		}
+	}
+}

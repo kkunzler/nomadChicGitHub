8b943c3c11c4817f2a9a469432afa0a85e94943e
IsabelBru <izzibru@gmail.com>
2015-08-12 13:46:36 -0700

    M/F button

--- nomadChic.js
+++ nomadChic.js
@@ -1,1 +1,5 @@
-api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+//api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+function packingList(){
+	var gender= document.getElementById("who").value;
+ 	console.log(gender);
+}	
